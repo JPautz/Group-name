a1180db653e5391dcc48cca51203813b0832cdad
Alexander Chamberlain <alxchamberlain@gmail.com>
2017-03-02 21:14:38 -0800

    Add imports for classes

--- src/main/java/base/student/Student.java
+++ src/main/java/base/student/Student.java
@@ -1,9 +1,12 @@
 package base.student;
 
+import base.flowchart.Flowchart;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.ArrayList;
 
 @Entity
 public class Student {
