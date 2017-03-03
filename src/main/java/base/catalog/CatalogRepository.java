e035d0821bfdce8ded9c737e62789640168330dd
Alexander Chamberlain <alxchamberlain@gmail.com>
2017-03-02 21:25:36 -0800

    Remove comment

--- src/main/java/base/catalog/CatalogRepository.java
+++ src/main/java/base/catalog/CatalogRepository.java
@@ -2,8 +2,5 @@ package base.catalog;
 
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by Stevie on 2017/03/03.
- */
 public interface CatalogRepository extends CrudRepository<Catalog, Long> {
 }
