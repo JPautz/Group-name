05d53a484ae66b2b29a0ec81ddacb81b01a15e49
Alexander Chamberlain <alxchamberlain@gmail.com>
2017-03-02 21:23:05 -0800

    Add more details to Year

--- src/main/java/base/year/Year.java
+++ src/main/java/base/year/Year.java
@@ -1,22 +1,37 @@
 package base.year;
 
+import base.quarter.Quarter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.ArrayList;
 
+@Entity
 public class Year {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
     private String date;
     private boolean showSummer;
+    private ArrayList<Quarter> quarters;
 
     public Year() {}
 
     public Year(String date, boolean showSummer) {
         this.date = date;
         this.showSummer = showSummer;
+        this.quarters = new ArrayList<Quarter>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getDate() { return date; }
@@ -27,5 +42,17 @@ public class Year {
 
     public void toggleSummer() {
         this.showSummer =  !this.showSummer;
+    }
+
+    public ArrayList<Quarter> getQuarters() {
+        return quarters;
+    }
+
+    public void addQuarter(Quarter quarter) {
+        quarters.add(quarter);
+    }
+
+    public void removeQuarter(Quarter quarter) {
+        quarters.remove(quarter.getId());
     }
 }
--- src/main/java/base/year/YearRepository.java
+++ src/main/java/base/year/YearRepository.java
@@ -3,6 +3,4 @@ package base.year;
 import org.springframework.data.repository.CrudRepository;
 
 public interface YearRepository extends CrudRepository<Year, Long> {
-
-
 }
