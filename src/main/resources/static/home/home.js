23313a2d193e0d5ec506ee2b876ae31d50fb82d3
Alexander Chamberlain <alxchamberlain@gmail.com>
2017-03-11 16:22:57 -0800

    Add angular hamburger menu

--- src/main/resources/static/home/home.js
+++ src/main/resources/static/home/home.js
@@ -1,21 +1,68 @@
 angular.module( 'sample.home', [
-  'ui.router',
-  'angular-storage',
-  'angular-jwt',
-  angularDragula(angular)
+    'ui.router',
+    'angular-storage',
+    'angular-jwt',
+    'ngMaterial',
+    angularDragula(angular)
 ])
-.config(function($stateProvider) {
-  $stateProvider.state('home', {
-    url: '/',
-    controller: 'HomeCtrl',
+.config(function($stateProvider, $mdThemingProvider) {
+    $stateProvider.state('home', {
+        url: '/',
+        controller: 'HomeCtrl',
     templateUrl: 'home/mustang.html',
-    data: {
-      requiresLogin: true
-    }
-  });
-})
-.controller( 'HomeCtrl', function HomeController( $scope, $http, store, jwtHelper) {
+        data: {
+            requiresLogin: true
+        }
+    });
 
-  $scope.jwt = store.get('jwt');
-  $scope.decodedJwt = $scope.jwt && jwtHelper.decodeToken($scope.jwt);
+    $mdThemingProvider.theme('default')
+        .primaryPalette('green');
+})
+.controller('HomeCtrl', function HomeController($scope, $http, store, jwtHelper, $mdSidenav, $log) {
+
+    $scope.jwt = store.get('jwt');
+    $scope.decodedJwt = $scope.jwt && jwtHelper.decodeToken($scope.jwt);
+
+    $scope.toggleLeft = buildToggler('left');
+        $scope.isOpenLeft = function(){
+            return $mdSidenav('left').isOpen();
+        };
+
+        /**
+         * Supplies a function that will continue to operate until the
+         * time is up.
+         */
+        function debounce(func, wait, context) {
+            var timer;
+
+            return function debounced() {
+                var context = $scope,
+                        args = Array.prototype.slice.call(arguments);
+                $timeout.cancel(timer);
+                timer = $timeout(function() {
+                    timer = undefined;
+                    func.apply(context, args);
+                }, wait || 10);
+            };
+        }
+
+        function buildToggler(navID) {
+            return function() {
+                // Component lookup should always be available since we are not using `ng-if`
+                $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                        $log.debug("toggle " + navID + " is done");
+                    });
+            };
+        }
+})
+.controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function () {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav('left').close()
+            .then(function () {
+                $log.debug("close LEFT is done");
+            });
+    };
 });
